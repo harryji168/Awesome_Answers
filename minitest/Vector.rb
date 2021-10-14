8acd4f783b34338de1f770128b5fba4c0e334229
Harry Ji <jiharry@hotmail.com>
2021-10-14 10:29:11 -0700

    mini testing

--- minitest/Vector.rb
+++ minitest/Vector.rb
@@ -8,4 +8,10 @@ class Vector
     def length
         Math.sqrt(@x ** 2 + @y ** 2)
     end
+
+    def to_s
+        "Vector (#{@x}, #{@y})"
+    end
+    
+    
 end
--- minitest/VectorTest.rb
+++ minitest/VectorTest.rb
@@ -2,5 +2,23 @@ require "minitest/autorun"
 require "./Vector.rb"
 
 class VectorTest < MiniTest::Test
+    def test_length
+        # GIVEN - the initial state of our program
+        # a vector of (3, 4)
+        vector = Vector.new(3 ,4)
+
+        # WHEN - an action is triggered
+        # Then length method is called
+        length_of_vector = vector.length
+
+        # THEN - we verify the final state
+        # The length should equal 5
+        assert_equal(5, length_of_vector)
+        
+        # When using assert_equal, the argument order is as follows
+        # 1 the value we expect or want
+        # 2 the actual value our code returned
+
+    end
     
 end
