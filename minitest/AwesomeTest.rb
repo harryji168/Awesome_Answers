98d343a7451772ee3c537d873b8e8636370e2d0a
Harry Ji <jiharry@hotmail.com>
2021-10-14 09:59:00 -0700

    testing

---
+++ minitest/AwesomeTest.rb
@@ -0,0 +1,13 @@
+require "minitest/autorun"
+
+
+# To create a grouping of tests with MiniTest,
+# create a class whose name ends in `Test` and inherits from MiniTest::Test
+class AwesomeTest < MiniTest::Test
+
+    def test_something
+        
+    end
+    
+
+end
